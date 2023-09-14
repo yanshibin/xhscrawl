a88e3bf846283c06b345393934321f2426ace15b
wangzhiyang <1162466610@qq.com>
2023-09-14 20:32:11 +0800

    update code

--- demo/xhs.py
+++ demo/xhs.py
@@ -1,12 +1,32 @@
 import json
-import pprint
-
+import os
 import re
 import execjs
-from py_mini_racer import py_mini_racer
+import requests
 
-def GetXs( cookie, api, data):
-    with open('xhs.js', 'r', encoding='utf-8') as f:
+headers = {
+    "accept": "application/json, text/plain, */*",
+    "accept-language": "zh-CN,zh;q=0.9",
+    "cache-control": "no-cache",
+    "content-type": "application/json;charset=UTF-8",
+    "origin": "https://www.xiaohongshu.com",
+    "pragma": "no-cache",
+    "referer": "https://www.xiaohongshu.com/",
+    "sec-ch-ua": "\"Chromium\";v=\"112\", \"Google Chrome\";v=\"112\", \"Not:A-Brand\";v=\"99\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-site",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
+    "x-b3-traceid": "a31fffc0ee4f5d8f",
+}
+
+
+def getXs(cookie, api, data):
+    current_directory = os.path.dirname(__file__)
+    file_path = os.path.join(current_directory, "xhs.js")
+    with open(file_path, 'r', encoding='utf-8') as f:
         jstext = f.read()
 
     ctx = execjs.compile(jstext)
@@ -22,10 +42,47 @@ def GetXs( cookie, api, data):
     result = ctx.call("get_xs", api, data, a1)
     return result
 
-# 向笔记发送评论
+
+def sentRequest(host, api, data, cookie):
+    xs_xt = getXs(cookie, api, data)
+
+    headers['cookie'] = cookie
+    headers['X-s'] = xs_xt['X-s']
+    headers['X-t'] = str(xs_xt['X-t'])
+
+    url = host + api
+
+    return requests.post(url=url, data=json.dumps(data, separators=(",", ":")), headers=headers)
+
+
+def DoApi(param, cookie):
+    api = '/api/sns/web/v1/comment/post'
+    host = 'https://edith.xiaohongshu.com'
+    data = {
+        "note_id": param["note_id"],
+        "content": param["content"],
+        "at_users":  param["at_users"],
+    }
+    return sentRequest(host, api, data, cookie)
+
+
+
+
 if __name__ == '__main__':
-    cookie = 'put your cookie here'
-    api = "/api/sns/web/v1/comment/post"  # comment api
-    data = {"note_id": "put your note id here", "content": "put your comment here", "at_users": []}
-    xs = GetXs(cookie, api, data)
-    print(json.dumps(xs, indent=4))
+
+
+    cookie = ""  # put your cookie here
+
+    param = {
+        "note_id": "64e1d603000000001700f40d",
+        "content": "hello world",
+        "at_users":  []
+    }
+
+    response = DoApi(param,cookie)
+    if response.status_code == 200:
+        print("Request successful:")
+        print(response.json())
+    else:
+        print("POST request failed. Status code:", response.status_code)
+        print(response.text)
